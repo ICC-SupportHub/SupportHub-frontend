66bdbcd470864e6ba96673790172e98d002ad2d2
dohee12 <happyddo42@gmail.com>
2025-07-21 13:20:12 +0900

    feat: 감정 기록 히스토리 및 시각화 차트 기능 추가

---
+++ src/app/emotion-history/page.jsx
@@ -0,0 +1,9 @@
+import EmotionHistory from "@/components/emotion-history"
+
+export default function HistoryPage() {
+  return (
+    <div className="flex-1 flex flex-col h-full">
+      <EmotionHistory />
+    </div>
+  )
+}
