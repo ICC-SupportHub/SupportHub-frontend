27718e21a35e30d55a1ee3bb5a1a898787b02a10
dohee <happyddo42@gmail.com>
2025-09-05 18:38:36 +0900

    Revert "Merge pull request #5 from ICC-SupportHub/dlawlsdyd"
    This reverts commit 5a9f239d4d6763e8f6112df48b07c828463b4dc4, reversing
    changes made to 6d0d242a00502c18aa0c79a1a9e94bc0863c645b.

--- src/app/emotion-diary/page.jsx
+++
@@ -1,9 +0,0 @@
-import EmotionDiary from "@/components/emotion-diary"
-
-export default function DiaryPage() {
-  return (
-    <div className="flex-1 flex flex-col h-full">
-      <EmotionDiary />
-    </div>
-  )
-}
